6d22eeff45e051b5b2a3cf3efc4f2c7156e4da53
Thang Mai <decenova@Thangs-MacBook-Pro.local>
2022-11-24 12:31:15 +0700

    Updates

---
+++ precache-manifest.a03d3088cafa7d5f55c0e24ab0d8c252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99c4e648a413417142e9eda6d219b0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd532789192e6c40b2f3",
+    "url": "./static/css/main.f1cc2d8c.chunk.css"
+  },
+  {
+    "revision": "aedcdfa4c78b847a8170",
+    "url": "./static/js/2.5e3595c7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "./static/js/2.5e3595c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd532789192e6c40b2f3",
+    "url": "./static/js/main.4de403ba.chunk.js"
+  },
+  {
+    "revision": "023eb42f596a51f8f4d5",
+    "url": "./static/js/runtime-main.fec35fcf.js"
+  },
+  {
+    "revision": "eb8e77793539364a6053487570ee2382",
+    "url": "./static/media/example.eb8e7779.png"
+  }
+]);
